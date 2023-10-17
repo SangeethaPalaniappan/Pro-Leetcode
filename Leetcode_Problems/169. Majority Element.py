a82818dfba7da78c0c18b07a444f59a17ee3390c
Sangeetha <sangeethapa2002@gmail.com>
2023-10-17 21:42:03 +0530

    File Modified

--- Leetcode_Problems/169. Majority Element.py
+++ Leetcode_Problems/169. Majority Element.py
@@ -1,15 +1,5 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
+        nums.sort()
         n = len(nums) // 2
-        arr = []
-        dic = {}
-        for i in nums:
-            if i not in dic:
-                dic[i] = 1
-            else:
-                dic[i] += 1
-        for d in dic.keys():
-            m = dic.get(d)
-            if m > n:
-                return d
-        
+        return nums[n]
