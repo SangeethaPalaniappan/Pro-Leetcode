cab958556d3aca258457489fab408159e43db50c
Sangeetha <sangeethapa2002@gmail.com>
2024-02-13 09:27:24 +0530

    Problem 2108

---
+++ Leetcode_Problems/2108. Find First Palindromic String in the Array.py
@@ -0,0 +1,15 @@
+class Solution:
+    def firstPalindrome(self, words):
+
+        for string in words:
+            start = 0
+            end = len(string) - 1
+            while start <= end:
+                if string[start] == string[end]:
+                    start += 1
+                    end -= 1
+                else:
+                    break
+            else:
+                return string            
+        return ""        
